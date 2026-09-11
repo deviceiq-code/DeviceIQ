bb48a028b54bc1b609b5722b0b2dc91dbc40b1b3
Fernando Almeida <fernando.almeida@me.com>
2026-09-11 09:58:47 -0300

    dev

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ void setup() {
     esp_log_level_set("*", ESP_LOG_NONE);
     
     Serial.begin(115200);
-    Serial.print("Starting DeviceIQ...\r\n\r\n");
+    Serial.print("Starting DeviceIQ...");
 
     App.Start();
 }
